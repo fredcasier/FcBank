91ec53a455caf60a4d9fe758a06db39bac19bed3
FrCa <fredericasier@gmail.com>
2026-08-13 17:24:30 +0200

    [Feature] CurrentAccount class

---
+++ src/main/components/CurrentAccount.java
@@ -0,0 +1,11 @@
+package main.components;
+
+
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
+public class CurrentAccount extends Account {
+
+    public CurrentAccount(String label, Client client) {
+        super(label, client);
+    }
+
+}
