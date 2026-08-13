b1991c684748f7b70ef909c1cf6a2ee30d0b3149
FrCa <fredericasier@gmail.com>
2026-08-13 17:24:53 +0200

    [Feature] SavingsAccount class

---
+++ src/main/components/SavingsAccount.java
@@ -0,0 +1,10 @@
+package main.components;
+
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
+public class SavingsAccount extends Account {
+
+    public SavingsAccount(String label, Client client) {
+        super(label, client);
+    }
+    
+}
