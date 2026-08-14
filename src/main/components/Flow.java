ff62c70d9b146e99468b55287297d3e3d720e497
FrCa <fredericasier@gmail.com>
2026-08-14 12:56:24 +0200

    [Feature] Flow class

---
+++ src/main/components/Flow.java
@@ -0,0 +1,70 @@
+package main.components;
+
+import java.util.Date;
+
+// 1.3.2 Creation of the Flow class
+public abstract class Flow {
+    private String comment;
+    private Integer identifier;
+    private Double amount;
+    private Integer targetAccountNumber;
+    private Boolean effect;
+    private Date flowDate;
+
+    public Flow(String comment, Integer identifier, Double amount, Integer targetAccountNumber, Boolean effect, Date flowDate) {
+        this.comment = comment;
+        this.identifier = identifier;
+        this.amount = amount;
+        this.targetAccountNumber = targetAccountNumber;
+        this.effect = effect;
+        this.flowDate = flowDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+        public Integer getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(Integer identifier) {
+        this.identifier = identifier;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Integer getTargetAccountNumber() {
+        return targetAccountNumber;
+    }
+
+    public void setTargetAccountNumber(Integer targetAccountNumber) {
+        this.targetAccountNumber = targetAccountNumber;
+    }
+
+    public Boolean getEffect() {
+        return effect;
+    }
+
+    public void setEffect(Boolean effect) {
+        this.effect = effect;
+    }
+
+    public Date getFlowDate() {
+        return flowDate;
+    }
+
+    public void setFlowDate(Date flowDate) {
+        this.flowDate = flowDate;
+    }
+}
