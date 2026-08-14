7b4ad6a1c493dc4db7fa7a92b431e0c7c1ff5e84
FrCa <fredericasier@gmail.com>
2026-08-14 11:28:11 +0200

    [Update] Main.java: Hashtable implemented

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,30 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 import main.components.*;
 
 // 1.1.2 Creation of Main class for tests
 // 1.2.3 Creation of the tablea account
+// 1.3.1 Adaptation of the table of accounts
 public class Main {
 
 
     public static void main(String[] args) {
         ArrayList<Client> clients;
         ArrayList<Account> accounts;
+        Hashtable<Integer, Account> accountsHashtable;
 
         clients = generateClients(4);
         displayClient(clients);
 
         accounts = generateAccounts(clients);
         displayAccounts(accounts);
+
+        accountsHashtable = genenateAccountHashtable(accounts);
+        displayAccountsHastable(accountsHashtable);
+
     }
 
     public static ArrayList<Client> generateClients(int number) {
@@ -41,11 +48,25 @@ public class Main {
         return accounts;
     }
 
+    public static Hashtable<Integer, Account> genenateAccountHashtable(ArrayList<Account> accounts) {
+        Hashtable<Integer, Account> accountsHashtable = new Hashtable<>();
+        for (Account account : accounts) {
+            accountsHashtable.put(account.getAccountNumber(), account);
+        }
+        return accountsHashtable;
+    }
+
     public static void displayClient(ArrayList<Client> clients) {
         clients.stream().forEach(System.out::println);
     }
 
     public static void displayAccounts(ArrayList<Account> accounts) {
         accounts.stream().forEach(System.out::println);
+    }
+
+    public static void displayAccountsHastable(Hashtable<Integer, Account> accountsHashtable) {
+        accountsHashtable.entrySet().stream()
+            .sorted((account1, account2) -> account1.getValue().getBalance().compareTo(account2.getValue().getBalance()))
+            .forEach(System.out::println);
     }
 }
